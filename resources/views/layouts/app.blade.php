971e38c9bf0a51e784bd7b97fc99c5dc3c1e5bdd
AbdiRabbani <abdirabbani59@gmail.com>
2023-04-08 22:48:16 +0700

    ui fixing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -179,25 +179,6 @@
 
         <!-- End Navbar -->
         @yield('content')
-
-        <footer class="footer py-4  ">
-            <div class="container-fluid">
-                <div class="row align-items-center justify-content-lg-between">
-                    <div class="col-lg-6 mb-lg-0 mb-4">
-                        <div class="copyright text-center text-sm text-muted text-lg-start">
-                            © <script>
-                                document.write(new Date().getFullYear())
-
-                            </script>,
-                            made by
-                            <a href="https://idn.sch.id/" class="font-weight-bold" target="_blank">IDN Boarding School
-                                Student</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
-
     </main>
 
     <!--   Core JS Files   -->
