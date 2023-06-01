12953f6a9d0fb64a3115d40e18c7c7533b0b811a
AbdiRabbani <abdirabbani59@gmail.com>
2023-06-01 13:02:00 +0700

    wardana's request update

--- resources/views/leave/create-leave.blade.php
+++ resources/views/leave/create-leave.blade.php
@@ -16,13 +16,15 @@
                 placeholder="Write your reason here">
 
             <label for="dataStart">leave start date *M/D/Y</label>
-            <input required type="date" name="start_leave" id="dataStart" class="form-control border p-2">
+            <input required type="date" onchange="dataSetEnd()" name="start_leave" id="dataStart" class="form-control border p-2">
 
             <label for="dataEnd">leave end date *M/D/Y</label>
-            <input required type="date" name="end_leave" id="dataEnd" class="form-control border p-2">
+            <input required type="date" onchange="dateset()" name="end_leave" id="dataEnd" class="form-control border p-2">
 
             <input required type="text" name="status" value="pending" id="" hidden>
             <input required type="text" name="id_luser" value="{{Auth::user()->id}}" id="" hidden>
+
+            <p>total <span id="span"></span> <span id="span2"></span></p>
 
             @foreach($user as $row)
             <input required type="text" name="id_manager" value="{{$row->dept->id_manager}}" id="" hidden>
@@ -32,11 +34,49 @@
 
             <div class="text-end mt-4 d-flex justify-content-end">
                 <p class="my-1 me-2">*The rest of your leave this year is {{$result}} day</p>
-                <button class="btn btn-success" id="send-leave">Send</button>
+                <button class="btn btn-success" type="submit" id="send-leave" onclick="sendLeave()">Send</button>
             </div>
         </form>
     </div>
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<script>
+
+    function dataSetEnd() {
+        const startdate =  document.getElementById('dataStart').value;
+        document.getElementById('dataEnd').min = new Date(startdate).toISOString().split("T")[0];
+    }
+
+    function dateset() {
+    const startdate =  document.getElementById('dataStart').value;
+    const enddate =  document.getElementById('dataEnd').value;
+
+    const date1 = new Date(startdate);
+    const date2 = new Date(enddate);
+
+    const time = Math.abs(date2 - date1 + 1);
+    const days = Math.ceil(time / (1000 * 60 * 60 * 24));
+
+
+    document.getElementById('span').innerText = days
+
+    if(days <= 12) {
+        document.getElementById("send-leave").setAttribute('type', 'submit');
+        document.getElementById("send-leave").classList.remove('btn-leave');
+    } else {
+        document.getElementById("send-leave").setAttribute('type', 'button');
+        document.getElementById("send-leave").classList.add('btn-leave');
+        Swal.fire(
+            'Wanna Leave?',
+            'You have use all your leave limmit',
+            'warning'
+        )
+    }
+   }
+   
+</script>
 
 <script>
     document.getElementById('dataStart').min = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000)
